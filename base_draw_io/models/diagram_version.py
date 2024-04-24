79c1b1ef6ae288a1fbc7d3e2daad053d20f80bca
Matt Harrison <matt@harrison.consulting>
2024-04-24 19:39:22 -0400

    Refactored DiagramVersion model to move naming code to models consuming it and removed task_id field from view.

--- base_draw_io/models/diagram_version.py
+++ base_draw_io/models/diagram_version.py
@@ -7,13 +7,5 @@ class DiagramVersion(models.Model):
     _order = "create_date DESC"
     _rec_name = "name"
 
-    name = fields.Char(copy=False, compute="_compute_name", store=True)
+    name = fields.Char()
     diagram_xml = fields.Text()
-
-    @api.depends('create_date')
-    def _compute_name(self):
-        for rec in self:
-            timezone = self._context.get('tz') or self.env.user.partner_id.tz or 'UTC'
-            self_tz = self.with_context(tz=timezone)
-            date = fields.Datetime.context_timestamp(self_tz, rec.create_date)
-            rec.name = _(f"{rec.task_id.name} - {date.strftime('%Y-%m-%d %H:%M:%S')}")
